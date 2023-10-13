f57c745ec01b03dae7853281d639f9c35993403b
Antti Kervinen <antti.kervinen@intel.com>
2023-10-13 13:01:53 +0300

    routines: add before and after timestamping to statactions

--- test/e2e/memtierd.test-suite/no-policy/n4c16/test00-routines/code.var.sh
+++ test/e2e/memtierd.test-suite/no-policy/n4c16/test00-routines/code.var.sh
@@ -43,3 +43,35 @@ vm-command "grep RoutineStatActions.command.memtier $MEMTIERD_OUTPUT | tail -n 1
 [[ $(awk '{print $1}' <<< "$COMMAND_OUTPUT") -gt 15 ]] || {
     command-error "at least 15 memtier command executions expected"
 }
+
+MEMTIERD_YAML="
+routines:
+  - name: statactions
+    config: |
+      intervalms: 1000
+      intervalcommandrunner: memtier-prompt
+      intervalcommand: [\"stats -t events\"]
+      timestamp: \"before: epochs: unix, unix.s, unix.milli, unix.micro and unix.nano or duration, duration.s, duration.milli, duration.micro, duration.nano\n
+          or a template: 2006-01-02T15:04.05 -0700 or a string like\n unixdate rfc822z rfc3339nano or rfc3339\n\"
+      timestampafter: \"after: rfc3339\n\"
+policy:
+  name: stub
+"
+memtierd-start
+sleep 2.5
+memtierd-stop
+vm-command "grep -E 'before: epochs: [12].*' $MEMTIERD_OUTPUT | tail -n 1" || {
+    command-error "could not find timestamp before command"
+}
+vm-command "grep -E 'after:' $MEMTIERD_OUTPUT | tail -n 1" || {
+    command-error "could not find timestamp after command"
+}
+vm-command "grep -B2 -E 'after:' $MEMTIERD_OUTPUT | tail -n 3 | grep RoutineStatActions" || {
+    command-error "could not find command output before timestamp after command"
+}
+vm-command "grep -A3 -E 'before: epochs: [12].*' $MEMTIERD_OUTPUT | tail -n 1 | grep 'table: events'" || {
+    command-error "table: events is missing three lines after before epochs"
+}
+vm-command "! ( grep nano $MEMTIERD_OUTPUT || grep rfc $MEMTIERD_OUTPUT || grep unix $MEMTIERD_OUTPUT || grep duration $MEMTIERD_OUTPUT ) " || {
+    command-error "timestamp words remained unreplaced"
+}
