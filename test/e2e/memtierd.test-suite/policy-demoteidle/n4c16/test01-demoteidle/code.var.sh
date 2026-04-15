cd13c1d57dcb0a18f276b9ed073817905417f6f4
Antti Kervinen <antti.kervinen@intel.com>
2026-04-15 11:49:23 +0300

    demote-idle: optimize scan/demote to activate mover early

--- test/e2e/memtierd.test-suite/policy-demoteidle/n4c16/test01-demoteidle/code.var.sh
+++ test/e2e/memtierd.test-suite/policy-demoteidle/n4c16/test01-demoteidle/code.var.sh
@@ -45,15 +45,22 @@ policy:
       bandwidth: 10000
 "
 memtierd-start
+MEMTIERD_START_TIME=$(date +%s)
 
 sleep 4
 
 echo "waiting 700M+ (idle) to be moved to node 3"
 memtierd-match-pagemoving "3\:0\.7[0-9][0-9]" 12 8
+PHASE1_DONE_TIME=$(date +%s)
+PHASE1_ELAPSED=$(( PHASE1_DONE_TIME - MEMTIERD_START_TIME ))
+echo "TIMING: phase 1 (idle demotion) completed in ${PHASE1_ELAPSED}s"
 
 echo "stop meme, expect remaining ~300M to be moved to node 3 too"
 vm-command "kill -STOP ${MEME_PID}"
 memtierd-match-pagemoving "3\:1\.[0-2]" 8 8
+PHASE2_DONE_TIME=$(date +%s)
+PHASE2_ELAPSED=$(( PHASE2_DONE_TIME - MEMTIERD_START_TIME ))
+echo "TIMING: phase 2 (full demotion) completed in ${PHASE2_ELAPSED}s"
 
 memtierd-stop
 memtierd-meme-stop
